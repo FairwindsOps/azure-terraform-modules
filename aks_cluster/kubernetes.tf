4169daf9a9825fd2216ca003264f3959f1da649f
Mary Goodhart <mary.loves.pry@gmail.com>
2020-06-23 08:15:06 -0600

    Adding load config false back for testing

--- aks_cluster/kubernetes.tf
+++ aks_cluster/kubernetes.tf
@@ -4,6 +4,7 @@ provider "kubernetes" {
   client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+  load_config_file = "false"
 }
 
 ## Create cluster role binding for the clusteradmin AAD group
